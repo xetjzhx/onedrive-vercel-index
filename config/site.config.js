bf8f1cc263e7541d7bd7678e09d33ba79740cda6
xetjzh <97598968+xetjzh@users.noreply.github.com>
2023-04-11 19:43:42 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -50,12 +50,12 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  /*links: [
+  links: [
     {
       name: 'GitHub',
       link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
-  ],*/
+  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
